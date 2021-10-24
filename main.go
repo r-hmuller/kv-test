ca2464216f136c6a13a02d060ce32c05cddd3a8e
Rodrigo Müller <rodrigo.muller@petlove.com.br>
2021-10-24 20:58:07 -0300

    Allow more concurrent connections.

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func (ex *Executor) monitorThroughput(ctx context.Context) error {
 func main() {
 	isBeingTested = false
 	app := fiber.New(fiber.Config{
-		Concurrency: 1024 * 1024,
+		Concurrency: 1024 * 1024 * 1024,
 		AppName:     "Test App v1.0.1",
 	})
 	var keyValueDB map[string]string
