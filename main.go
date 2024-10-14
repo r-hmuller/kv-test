8c9d4977002da0df7610fe3f5612c9d950267728
Rodrigo Muller <r.muller@global.leaseweb.com>
2024-10-14 20:23:55 +0100

    Remove file for testing

--- main.go
+++ main.go
@@ -1,62 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"log"
 	"math/rand"
 	"os"
 	"strconv"
 	"sync"
 	"sync/atomic"
-	"time"
 )
 
 var isBeingTested atomic.Bool
 var vazao []uint32
-
-type Executor struct {
-	cancel    context.CancelFunc
-	logFile   *os.File
-	batchLat  time.Time
-	measuring bool
-	thrCount  uint32 // atomic
-	t         *time.Ticker
-}
-
-func NewExecutor() (*Executor, error) {
-	ctx, cn := context.WithCancel(context.Background())
-	fn := "/data/logfile.log"
-	flags := os.O_CREATE | os.O_TRUNC | os.O_WRONLY | os.O_APPEND | os.O_SYNC
-	ex := &Executor{
-		cancel: cn,
-		t:      time.NewTicker(time.Second),
-	}
-	var err error
-	ex.logFile, err = os.OpenFile(fn, flags, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	go ex.monitorThroughput(ctx)
-	return ex, nil
-}
-
-func (ex *Executor) monitorThroughput(ctx context.Context) error {
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-
-		case <-ex.t.C:
-			if isBeingTested.Load() {
-				t := atomic.SwapUint32(&ex.thrCount, 0)
-				vazao = append(vazao, t)
-			}
-		}
-	}
-}
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
@@ -77,11 +31,6 @@ func main() {
 		AppName:     "Test App v1.0.1",
 	})
 
-	ex, err := NewExecutor()
-	if err != nil {
-		panic(err)
-	}
-
 	app.Get("/", func(c *fiber.Ctx) error {
 		key, err := strconv.Atoi(c.Query("key"))
 		if err != nil {
@@ -89,10 +38,8 @@ func main() {
 		}
 		value, ok := concurrentMap.Load(key)
 		if ok {
-			atomic.AddUint32(&ex.thrCount, 1)
 			return c.JSON(value)
 		}
-		atomic.AddUint32(&ex.thrCount, 1)
 		return c.Status(404).JSON("Key not found")
 	})
 
@@ -103,11 +50,9 @@ func main() {
 		}{}
 
 		if err := c.BodyParser(&payload); err != nil {
-			atomic.AddUint32(&ex.thrCount, 1)
 			return c.Status(400).JSON("Error when trying to decode the payload")
 		}
 		concurrentMap.Store(payload.Key, payload.Value)
-		atomic.AddUint32(&ex.thrCount, 1)
 		return c.Status(204).JSON("")
 	})
 
@@ -125,26 +70,6 @@ func main() {
 		}
 		if payload.Action == "stop" {
 			isBeingTested.Store(false)
-			flags := os.O_CREATE | os.O_TRUNC | os.O_WRONLY | os.O_APPEND | os.O_SYNC
-			customLog, err := os.OpenFile(payload.Path, flags, 0600)
-			if err != nil {
-				log.Print(err)
-				return c.Status(500).JSON(err.Error())
-			}
-
-			if err != nil {
-				log.Print(err)
-				return c.Status(500).JSON(err.Error())
-			}
-
-			for _, v := range vazao {
-				_, err := fmt.Fprintf(customLog, "%d\n", v)
-				if err != nil {
-					log.Print(err)
-					return c.Status(500).JSON(err.Error())
-				}
-			}
-
 			vazao = make([]uint32, 0)
 		}
 
@@ -173,7 +98,6 @@ func main() {
 		}
 
 		concurrentMap.Delete(key)
-		atomic.AddUint32(&ex.thrCount, 1)
 		return c.Status(204).JSON("")
 	})
 
@@ -181,7 +105,7 @@ func main() {
 	if portEnv == "" {
 		panic("Couldn't find PORT env")
 	}
-	err = app.Listen(portEnv)
+	err := app.Listen(portEnv)
 	if err != nil {
 		panic(err)
 	}
